6922df1e9b2755f69f387ad620caa94042336254
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 00:27:07 +0530

    Update watcher.py

--- VIPMUSIC/plugins/misc/watcher.py
+++ VIPMUSIC/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VIPMUSIC import app
-from VIPMUSIC.core.call import VIP
+from THUNDERMUSIC import app
+from THUNDERMUSIC.core.call import VIP
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await VIP.stop_stream_force(message.chat.id)
+    await THUNDER.stop_stream_force(message.chat.id)
