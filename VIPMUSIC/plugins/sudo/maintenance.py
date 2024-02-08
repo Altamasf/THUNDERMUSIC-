e56563f2c100705ac121c8156dbeb3159120d2fb
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 00:42:38 +0530

    Update maintenance.py

--- VIPMUSIC/plugins/sudo/maintenance.py
+++ VIPMUSIC/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VIPMUSIC import app
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils.database import (
+from THUNDERMUSIC import app
+from THUNDERMUSIC.misc import SUDOERS
+from THUNDERMUSIC.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
