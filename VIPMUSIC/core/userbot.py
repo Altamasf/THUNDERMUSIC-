e002a80340339fb016a747dccff17c485b9fded3
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-08 23:01:17 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -11,7 +11,7 @@ from ..logging import LOGGER
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 STRING_SESSION = getenv("STRING_SESSION", "")
-TEST_ID = int("-1002090555194")
+TEST_ID = int("-1002023557829")
 
 assistants = []
 assistantids = []
@@ -62,11 +62,11 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("SHIVANSH474")
-                await self.one.join_chat("SHIVANSH474")
-                await self.one.join_chat("mastiwithfriendsx")
-                await self.one.join_chat("mastiwithfriendsx")
-                await self.one.join_chat("mastiwithfriendsx")
+                await self.one.join_chat("itz_R2h")
+                await self.one.join_chat("itz_dpz")
+                await self.one.join_chat("R2hxManagementBot")
+                await self.one.join_chat("itz_R2h")
+                await self.one.join_chat("About_Alt")
             except:
                 pass
             assistants.append(1)
@@ -94,10 +94,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("SHIVANSH474")
-                await self.two.join_chat("SHIVANSH474")
-                await self.two.join_chat("mastiwithfriendsx")
-                await self.two.join_chat("mastiwithfriendsx")
+                await self.two.join_chat("itz_R2h")
+                await self.two.join_chat("About_Alt")
+                await self.two.join_chat("itz_dpz")
+                await self.two.join_chat("Bezubaan_ishqz")
             except:
                 pass
             assistants.append(2)
@@ -117,10 +117,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("SHIVANSH474")
-                await self.three.join_chat("SHIVANSH474")
-                await self.three.join_chat("mastiwithfriendsx")
-                await self.three.join_chat("mastiwithfriendsx")
+                await self.three.join_chat("itz_R2h")
+                await self.three.join_chat("About_Alt")
+                await self.three.join_chat("itz_dpz")
+                await self.three.join_chat("Bezubaan_ishqz")
             except:
                 pass
             assistants.append(3)
@@ -140,10 +140,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("SHIVANSH474")
-                await self.four.join_chat("SHIVANSH474")
-                await self.four.join_chat("mastiwithfriendsx")
-                await self.four.join_chat("mastiwithfriendsx")
+                await self.four.join_chat("itz_R2h")
+                await self.four.join_chat("About_Alt")
+                await self.four.join_chat("itz_dpz")
+                await self.four.join_chat("Bezubaan_ishqz")
             except:
                 pass
             assistants.append(4)
@@ -163,10 +163,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("SHIVANSH474")
-                await self.five.join_chat("SHIVANSH474")
-                await self.five.join_chat("mastiwithfriendsx")
-                await self.five.join_chat("mastiwithfriendsx")
+                await self.five.join_chat("itz_R2h")
+                await self.five.join_chat("About_Alt")
+                await self.five.join_chat("itz_dpz")
+                await self.five.join_chat("Bezubaan_ishqz")
             except:
                 pass
             assistants.append(5)
