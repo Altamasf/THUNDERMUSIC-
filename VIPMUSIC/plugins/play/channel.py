be6816d4d683940b7dc7a903f7ce1d847973271f
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 00:28:21 +0530

    Update channel.py

--- VIPMUSIC/plugins/play/channel.py
+++ VIPMUSIC/plugins/play/channel.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
-from VIPMUSIC import app
-from VIPMUSIC.utils.database import set_cmode
-from VIPMUSIC.utils.decorators.admins import AdminActual
+from THUNDERMUSIC import app
+from THUNDERMUSIC.utils.database import set_cmode
+from THUNDERMUSIC.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
 
 
