882710b42cc5007e4d4bb4a4ea05c14f0e50d6e8
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 00:54:24 +0530

    Update gitinfo.py

--- VIPMUSIC/plugins/tools/gitinfo.py
+++ VIPMUSIC/plugins/tools/gitinfo.py
@@ -1,12 +1,12 @@
 import aiohttp
 from pyrogram import filters
-from VIPMUSIC import app
+from THUNDERMUSIC import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git itzshukla")
+        await message.reply_text("/git Altamasf")
         return
 
     username = message.text.split(None, 1)[1]
