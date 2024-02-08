0f01e4896eb5d704ddea24c10c1dbca6ea5eaed2
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-08 23:39:15 +0530

    Update resume.py

--- VIPMUSIC/plugins/admins/resume.py
+++ VIPMUSIC/plugins/admins/resume.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from VIPMUSIC import app
-from VIPMUSIC.core.call import VIP
-from VIPMUSIC.utils.database import is_music_playing, music_on
-from VIPMUSIC.utils.decorators import AdminRightsCheck
-from VIPMUSIC.utils.inline import close_markup
+from THUNDERMUSIC import app
+from THUNDERMUSIC.core.call import VIP
+from THUNDERMUSIC.utils.database import is_music_playing, music_on
+from THUNDERMUSIC.utils.decorators import AdminRightsCheck
+from THUNDERMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await VIP.resume_stream(chat_id)
+    await THUNDER.resume_stream(chat_id)
     buttons_resume = [
         [
             
