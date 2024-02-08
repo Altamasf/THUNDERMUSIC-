f7e3374f218e9cd0f7d3c6e63c219d0609a68d7d
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 00:38:26 +0530

    Update blchat.py

--- VIPMUSIC/plugins/sudo/blchat.py
+++ VIPMUSIC/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VIPMUSIC import app
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from VIPMUSIC.utils.decorators.language import language
+from THUNDERMUSIC import app
+from THUNDERMUSIC.misc import SUDOERS
+from THUNDERMUSIC.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from THUNDERMUSIC.utils.decorators.language import language
 from config import BANNED_USERS
 
 
