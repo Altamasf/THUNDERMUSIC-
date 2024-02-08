5971fbd6d80eba59d3aff12e0f3e3af8948dfce2
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-08 22:49:05 +0530

    Update mongo.py

--- VIPMUSIC/core/mongo.py
+++ VIPMUSIC/core/mongo.py
@@ -11,10 +11,10 @@ TEMP_MONGODB = "mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp
 
 if config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
-        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝗦𝗛𝗜𝗩𝗔𝗡𝗦𝗛 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
+        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐓𝐇𝐔𝐍𝐃𝐄𝐑 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
     )
     temp_client = Client(
-        "VIPMUSIC",
+        "THUNDERMUSIC",
         bot_token=config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
@@ -30,5 +30,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.VIPMUSIC
-    pymongodb = _mongo_sync_.VIPMUSIC
+    mongodb = _mongo_async_.THUNDERMUSIC
+    pymongodb = _mongo_sync_.THUNDERMUSIC
