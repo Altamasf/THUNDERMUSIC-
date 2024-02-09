e3e5c8bece08713d77cb2b51dd8d6f3f755f236d
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 09:26:16 +0530

    Update stream.py

--- VIPMUSIC/utils/stream/stream.py
+++ VIPMUSIC/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from VIPMUSIC import Carbon, YouTube, app
-from VIPMUSIC.core.call import VIP
-from VIPMUSIC.misc import db
-from VIPMUSIC.utils.database import add_active_video_chat, is_active_chat
-from VIPMUSIC.utils.exceptions import AssistantErr
-from VIPMUSIC.utils.inline import aq_markup, close_markup, stream_markup
-from VIPMUSIC.utils.pastebin import VIPBin
-from VIPMUSIC.utils.stream.queue import put_queue, put_queue_index
-from VIPMUSIC.utils.thumbnails import get_thumb
+from THUNDERMUSIC import Carbon, YouTube, app
+from THUNDERMUSIC.core.call import THUNDER
+from THUNDERMUSIC.misc import db
+from THUNDERMUSIC.utils.database import add_active_video_chat, is_active_chat
+from THUNDERMUSIC.utils.exceptions import AssistantErr
+from THUNDERMUSIC.utils.inline import aq_markup, close_markup, stream_markup
+from THUNDERMUSIC.utils.pastebin import THUNDERBin
+from THUNDERMUSIC.utils.stream.queue import put_queue, put_queue_index
+from THUNDERMUSIC.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await VIP.force_stop_stream(chat_id)
+        await THUNDER.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -79,7 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await VIP.join_call(
+                await THUNDER.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await VIPBin(msg)
+            link = await THUNDERBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -165,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await VIP.join_call(
+            await THUNDER.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -333,7 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await VIP.join_call(
+            await THUNDER.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await VIP.join_call(
+            await THUNDER.join_call(
                 chat_id,
                 original_chat_id,
                 link,
