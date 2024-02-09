66fa87e9fdd71cc946a158d1fcbc872a14d8e606
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 19:01:39 +0530

    Create __init__.py

---
+++ THUNDERMUSIC-/__init__.py
@@ -0,0 +1,29 @@
+from THUNDERMUSIC.core.bot import THUNDER
+from THUNDERMUSIC.core.dir import dirr
+from THUNDERMUSIC.core.git import git
+from THUNDERMUSIC.core.userbot import Userbot
+from THUNDERMUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = THUNDER()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+￼Enter
