6631cbcbe6bfd069589001ac50fd62a7d56d243c
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 09:14:20 +0530

    Update play.py

--- VIPMUSIC/utils/decorators/play.py
+++ VIPMUSIC/utils/decorators/play.py
@@ -9,9 +9,9 @@ from pyrogram.errors import (
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from VIPMUSIC import YouTube, app
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils.database import (
+from THUNDERMUSIC import YouTube, app
+from THUNDERMUSIC.misc import SUDOERS
+from THUNDERMUSIC.utils.database import (
     get_assistant,
     get_cmode,
     get_lang,
@@ -20,7 +20,7 @@ from VIPMUSIC.utils.database import (
     is_active_chat,
     is_maintenance,
 )
-from VIPMUSIC.utils.inline import botplaylist_markup
+from THUNDERMUSIC.utils.inline import botplaylist_markup
 from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
 
@@ -37,7 +37,7 @@ def PlayWrapper(command):
                     [
                         InlineKeyboardButton(
                             text="ʜᴏᴡ ᴛᴏ ғɪx ?",
-                            callback_data="VIPmousAdmin",
+                            callback_data="THUNDERmousAdmin",
                         ),
                     ]
                 ]
