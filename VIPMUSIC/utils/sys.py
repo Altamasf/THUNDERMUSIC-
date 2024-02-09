55c3b86a267b1382f6cced660ecf9d1e5cbd4fbd
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 09:36:04 +0530

    Update sys.py

--- VIPMUSIC/utils/sys.py
+++ VIPMUSIC/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from VIPMUSIC.misc import _boot_
-from VIPMUSIC.utils.formatters import get_readable_time
+from THUNDERMUSIC.misc import _boot_
+from THUNDERMUSIC.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
