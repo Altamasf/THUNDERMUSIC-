9d955db441543f839ef309034b31ad6d2b5a8d6d
Altamasf <158587853+Altamasf@users.noreply.github.com>
2024-02-09 09:34:20 +0530

    Update mongo.py

--- VIPMUSIC/utils/mongo.py
+++ VIPMUSIC/utils/mongo.py
@@ -7,7 +7,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.VIPMUSIC
+db = mongo.THUNDERMUSIC
 
 coupledb = db.couple
 
